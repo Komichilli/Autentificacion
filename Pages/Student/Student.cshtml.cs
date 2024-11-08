7e5e67eb8f174d6d51f7a706102e56b09f9278ab
Komichilli <victormarin3890@gmail.com>
2024-11-08 11:03:40 -0500

    Agregar Opciones al Nav

---
+++ Pages/Student/Student.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Autentificacion.Pages.Student
+{
+    public class StudentModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Subject/Subject.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Autentificacion.Pages.Subject
+{
+    public class SubjectModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
